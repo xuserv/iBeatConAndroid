5fe6f076ead1b475175d31f8e4a6184c26d8b2fa
xuserv <me@xuserv.net>
2014-04-30 19:46:26 +0900

    Add description and translations (using Google Translate)

--- gen/com/kuna/ibeatcon_android/R.java
+++ gen/com/kuna/ibeatcon_android/R.java
@@ -85,36 +85,45 @@ public final class R {
         public static final int action_reconnect=0x7f050002;
         public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050001;
-        public static final int desc_logo=0x7f05001b;
+        public static final int desc_logo=0x7f050024;
         /** Replace placeholder ID with your tracking ID
          */
         public static final int ga_trackingId=0x7f050000;
-        public static final int int_zoom=0x7f05001a;
+        public static final int int_zoom=0x7f050023;
         public static final int pref_misc=0x7f05000b;
         public static final int pref_network=0x7f050009;
         public static final int pref_skin=0x7f05000a;
         public static final int str_2p=0x7f05000f;
         public static final int str_bk=0x7f050014;
+        public static final int str_bk_desc=0x7f05001d;
         public static final int str_bp=0x7f050012;
+        public static final int str_bp_desc=0x7f05001c;
         public static final int str_connfail=0x7f050008;
         public static final int str_default=0x7f05000e;
-        public static final int str_dev=0x7f05001c;
-        public static final int str_developer=0x7f05001d;
+        public static final int str_dev=0x7f050025;
+        public static final int str_developer=0x7f050026;
         public static final int str_fb=0x7f050015;
+        public static final int str_fb_desc=0x7f05001e;
         public static final int str_firstrun=0x7f050007;
         public static final int str_hw=0x7f050016;
+        public static final int str_hw_desc=0x7f05001f;
         public static final int str_ip=0x7f05000c;
+        public static final int str_ip_desc=0x7f050018;
+        public static final int str_ip_input=0x7f050017;
         public static final int str_ko=0x7f050010;
-        public static final int str_maintain=0x7f05001e;
+        public static final int str_maintain=0x7f050027;
         public static final int str_modesel=0x7f05000d;
-        public static final int str_modifier=0x7f05001f;
+        public static final int str_modesel_desc=0x7f05001a;
+        public static final int str_modifier=0x7f050028;
         public static final int str_ns=0x7f050013;
-        public static final int str_origdev=0x7f050021;
-        public static final int str_original=0x7f050020;
-        public static final int str_reset=0x7f050018;
-        public static final int str_reset_msg=0x7f050019;
+        public static final int str_ns_desc=0x7f050019;
+        public static final int str_origdev=0x7f05002a;
+        public static final int str_original=0x7f050029;
+        public static final int str_reset=0x7f050021;
+        public static final int str_reset_msg=0x7f050022;
         public static final int str_so=0x7f050011;
-        public static final int str_zoom=0x7f050017;
+        public static final int str_zoom=0x7f050020;
+        public static final int str_zoom_desc=0x7f05001b;
     }
     public static final class style {
         /** 
